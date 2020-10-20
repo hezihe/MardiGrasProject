04206127ce04ef783cb3e1cb2a7313b6e6279c48
Hezi Henen <ezequielhenen@hotmail.com>
2020-10-20 16:34:30 +0300

    20_10_2020 - javascript (home.js, contactUs.js, trivia.js, quizPopUp.js) added instead of scirpt tag in html pages custom script removed from following pages: about.html, faq.html, media.html since is not in use

---
+++ src/home.js
@@ -0,0 +1,8 @@
+   function openBannerPopup(popUpUrl) {
+       window.open(popUpUrl, "_blank",
+           "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=500,height=500");
+   }
+
+   function OpenExternalLinkBanner(externalBannerUrl) {
+       window.open(externalBannerUrl, "_blank");
+   }
---
+++ src/trivia.js
@@ -0,0 +1,4 @@
+function openBannerPopup(popUpUrl) {
+    window.open(popUpUrl, "_blank",
+        "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=800,height=550");
+}
